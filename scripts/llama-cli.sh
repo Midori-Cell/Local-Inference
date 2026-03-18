9b5ca0bc11385fa217d0a67d1a5cb0516b24e8c4
Midori <148465040+Midori-Cell@users.noreply.github.com>
2026-03-18 11:00:21 +0100

    formatting llama-cli.sh

--- scripts/llama-cli.sh
+++ scripts/llama-cli.sh
@@ -1,10 +1,10 @@
 ~/llama.cpp/llama-cli \
         --model ~/models/llama-4-scout-q4_k_s.gguf \
         --threads 15 \
-	--tensor-split 1/1/1/1 \
+		--tensor-split 1/1/1/1 \
         --poll 100 \
         --n-gpu-layers 999 \
         --cache-type-k q4_0 \
         --cache-type-v f16 \
         --flash-attn 1 \
-	--ctx-size 16384 \
+		--ctx-size 16384 \
